bb2ca39f169ee4d93e06923f6efa748ce7278377
Kiran Panesar <kiran@mobilexlabs.com>
2016-11-16 19:12:03 -0600

    fixed link generator

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ window.addEventListener('input', function (e) {
  var android_url_field = document.getElementById("android_url");
 
  var final_url_field = document.getElementById("redirect_link");
- final_url_field.value = "https://"+ window.location.hostname + "/go/?ios=" + window.btoa(ios_url_field.value) + "&android="+ window.btoa(android_url_field.value);
+ final_url_field.value = "https://"+ window.location + "/go/?ios=" + window.btoa(ios_url_field.value) + "&android="+ window.btoa(android_url_field.value);
 
 }, false);
